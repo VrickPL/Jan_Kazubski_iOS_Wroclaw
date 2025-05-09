e34edce3f805e875ba274b2245f053fcee0142dd
Jan Kazubski <jkazubski@icloud.com>
2025-05-09 20:27:16 +0200

    Initial project

---
+++ ShoppingApp/ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Jan Kazubski on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
